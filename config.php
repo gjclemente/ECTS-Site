2b36cfe67f6efe1ca31d46e59b8f113aad2efee9
Gabby Clemente <44914621+gjclemente@users.noreply.github.com>
2021-05-06 13:44:51 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+﻿<?php
+    $DB_SERVER = '';
+    $DB_NAME   = '';
+    $DB_USERNAME = '';
+    $DB_PASSWORD = '';
+    $charset = 'utf8mb4';
+
+
+    $dsn = "mysql:host=$DB_SERVER;dbname=$DB_NAME;charset=$charset";
+
+
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+
+
+    try {
+            $link = new PDO($dsn, $DB_USERNAME, $DB_PASSWORD, $options);
+    } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+?>
