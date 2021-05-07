47a46c2b1d98148363b8b25e380a1e78a6ec6e12
Gabriella Clemente <CMP_GaClemente@ects.org>
2021-05-07 12:29:17 -0400

    another commit

---
+++ ECTSsite-create-tables-script.sql
@@ -0,0 +1,2 @@
+CREATE TABLE CMP_Student_Reviews (ID INT(6) auto_increment PRIMARY KEY, review_text VARCHAR(500),
+student_name VARCHAR(30), student_year INT, active INT DEFAULT 1);
---
+++ ECTSsite-populate-tables-script.sql
@@ -0,0 +1,7 @@
+INSERT INTO CMP_Student_Reviews (review_text, student_name, student_year) VALUES 
+("Lorem ipsum dolor sit amet, consectetur adipiscin est laborum.", "student", 2018),
+("Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "another student name", 2020),
+("Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", "a third student name", 2021),
+("sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "a fourth student name", 2000),
+("minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo fugiat nulla pariatur.", "a fifth student name", 2005),
+("incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "a final student name", 2008);
